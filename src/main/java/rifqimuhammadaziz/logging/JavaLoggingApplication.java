890741f061d6e5a5513622479892b120e1e3095e
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-01 10:22:19 +0700

    create maven project

---
+++ src/main/java/rifqimuhammadaziz/logging/JavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rifqimuhammadaziz/logging/JavaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
